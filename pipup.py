038ff54504e639576821caaa1acfeaf81c65cbfd
Pig Monkey <pm@pig-monkey.com>
2012-08-15 10:02:02 -0700

    pipup.py: Add a flag to only output packages with updates
    This is useful if you run the script in cron.

--- pipup.py
+++ pipup.py
@@ -6,6 +6,17 @@
 
 import xmlrpclib
 import pip
+import argparse
+
+# Set arguments.
+parser = argparse.ArgumentParser(description='Check if a newer version of \
+                                              installed packages are \
+                                              available on PyPI.')
+parser.add_argument('-q', '--quiet', action='store_true', dest='quiet',
+                    help='Only output packages with available updates.')
+
+# Parse arguments.
+args = parser.parse_args()
 
 pypi = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
 for dist in pip.get_installed_distributions():
@@ -13,12 +24,19 @@ for dist in pip.get_installed_distributions():
     if not available:
         # Try to capitalize pkg name
         available = pypi.package_releases(dist.project_name.capitalize())
-        
+
     if not available:
-        msg = 'no releases at pypi'
+        if args.quiet:
+            continue
+        else:
+            msg = 'no releases at pypi'
+        
     elif available[0] != dist.version:
         msg = '{} available'.format(available[0])
     else:
-        msg = 'up to date'
+        if args.quiet:
+            continue
+        else:
+            msg = 'up to date'
     pkg_info = '{dist.project_name} {dist.version}'.format(dist=dist)
     print '{pkg_info:40} {msg}'.format(pkg_info=pkg_info, msg=msg)
