a435246b8ef5e1cef2aa84b1c91b7f8b0e4ec467
Pig Monkey <pm@pig-monkey.com>
2012-11-08 10:27:57 -0800

    Improve mailcheck flexibility
    Now, when the persistent process is started, it always uses the quiet
    interface.
    
    When the non-persistent process is started, it passes along any extra
    arguments that the script was started with. Thus, if you only want to do
    a single quick sync, you could do something like:
    
        $ mailcheck.sh -q
    
    If you want to do a full sync, you can still do a simple:
    
        $ mailcheck.sh

--- mailcheck.sh
+++ mailcheck.sh
@@ -2,14 +2,14 @@
 # Check for new mail with OfflineImap
 
 # Define the location of OfflineImap, along with any options.
-OFFLINEIMAP='/usr/bin/offlineimap -u quiet'
+OFFLINEIMAP='/usr/bin/offlineimap'
 
 
 # If there are no OfflineImap processes running, start a continuously running
 # background process.
 if ! ps ax | grep -v grep | grep -q "`echo $OFFLINEIMAP | sed 's/ .*//'`"; then
-    $OFFLINEIMAP&
+    $OFFLINEIMAP -u quiet&
 # If a process is already running, run OfflineImap once.
 else
-    $OFFLINEIMAP -o
+    $OFFLINEIMAP -o $@
 fi
