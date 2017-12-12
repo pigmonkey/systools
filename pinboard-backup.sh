17931d5083a89604328c5834e4e5577239db9bc8
Pig Monkey <pm@pig-monkey.com>
2017-12-12 11:20:43 -0800

    pinboard: integrate git-annex

--- pinboard-backup.sh
+++ pinboard-backup.sh
@@ -3,11 +3,14 @@
 source ~/.keys/creds.sh
 
 tmp=$(mktemp)
-destination="$HOME/documents/pinboard.json"
+destination="$HOME/library/conf/"
 
 if curl -Ss "https://api.pinboard.in/v1/posts/all?auth_token=$PINBOARD_USER:$PINBOARD_KEY&format=json" -o "$tmp"; then
     if python -m json.tool "$tmp" >> /dev/null; then
-        mv "$tmp" "$destination"
+        cd "$destination"
+        git annex edit pinboard.json
+        mv "$tmp" "$destination/pinboard.json"
+        git annex sync -m "pinboard update"
     else
         echo "Output does not appear to be valid JSON: $tmp"
         exit 3
