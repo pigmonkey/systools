c33c2f167a67790905286165671ecbdec342dfc7
Pig Monkey <pm@pig-monkey.com>
2018-09-23 21:33:20 -0700

    add bookmark archiver

--- pinboard-backup.sh
+++ pinboard-backup.sh
@@ -11,6 +11,7 @@ if curl -Ss "https://api.pinboard.in/v1/posts/all?auth_token=$PINBOARD_USER:$PIN
         git annex edit pinboard.json
         mv "$tmp" "$destination/pinboard.json"
         git annex sync -m "pinboard update"
+        $HOME/bin/bookmark-archiver
     else
         echo "Output does not appear to be valid JSON: $tmp"
         exit 3
